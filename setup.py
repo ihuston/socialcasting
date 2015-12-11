127f23f973df602aaf4f052970efd180a588a998
Ian Huston <ihuston@pivotal.io>
2015-12-11 11:17:48 +0000

    Update setup.

--- setup.py
+++ setup.py
@@ -3,4 +3,7 @@ setup(
     name="socialcasting",
     version="0.1",
     packages=find_packages(),
+    install_requires=['pandas', 'requests'],
+    setup_requires=['pytest-runner'],
+    tests_requires=['pytest', 'responses']
 )
